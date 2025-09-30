0cb9ffa5649fe7ce32e88bc3dea997be0bfbda41
Abu Turab <turababu989@gmail.com>
2025-09-30 23:33:36 +0530

    Updated

--- app.js
+++ app.js
@@ -28,7 +28,7 @@ app.use(authRouter);
 app.use(router);
 
 
-const PORT = process.env.PORT;
+const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log(`Server is running on PORT :${PORT}`);
 });
