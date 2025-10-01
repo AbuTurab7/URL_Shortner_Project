fa8f1041c84b9a8c5b8f2fded8e9dd84e0fc7aaa
Abu Turab <turababu989@gmail.com>
2025-10-01 11:01:02 +0530

    Updated

--- app.js
+++ app.js
@@ -62,6 +62,7 @@ app.get("/", (req, res) => {
 
 console.log("ENV PORT from Railway:", process.env.PORT);
 const PORT = process.env.PORT || 8080;
-app.listen(PORT, () => {
+app.listen(PORT, "0.0.0.0", () => {
   console.log(`Server is running on PORT : ${PORT}`);
 });
+
